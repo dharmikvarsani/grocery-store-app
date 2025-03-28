b31cf11e9370d1d26d160e09fd6fff2a0de444d1
dharmikvarsani <dharmikvarsani968@gmail.com>
2025-03-28 15:02:11 +0530

    'change-in-next-config'

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost",
-          port: "1337",
-          pathname: "/uploads/**",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "grocery-store-admin.up.railway.app", 
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
